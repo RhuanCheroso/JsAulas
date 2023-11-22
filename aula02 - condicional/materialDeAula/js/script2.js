b4a62ab13ba864e72e452964dc3cc78a8a864a5f
RhuanCheroso <rhuanbezerra12@gmail.com>
2023-11-22 16:39:59 -0300

    aula02 - condicional

---
+++ aula02 - condicional/materialDeAula/js/script2.js
@@ -0,0 +1,46 @@
+var numero1 = Number(prompt("Digite aqui seu primeiro numero"));
+var operador = prompt("Digite a operação matemática");
+var numero2 = Number(prompt("Digite aqui seu segundo numero"));
+
+// escolha (var)
+// caso "isso"
+// faz aquilo
+// caso "teste"
+// faz uma coisa
+// fimFaca
+
+function calcularNovamente(){
+    var novoCalculo = confirm("Deseja fazer um novo calculo?");
+    if(novoCalculo == true){
+        location.reload();
+    }
+}
+
+switch(operador){
+    case "+":
+        var resultado = numero1 + numero2;
+        alert(resultado);
+
+       calcularNovamente();
+        break;
+    case  "-":
+        var resultado = numero1 - numero2;
+        alert(resultado);
+        calcularNovamente();
+        break;
+    case "*":
+        var resultado = numero1 * numero2;
+        alert(resultado);
+        calcularNovamente();
+        break;
+    case "/":
+        var resultado = numero1 / numero2;
+        alert(resultado);
+        calcularNovamente();
+        break;
+    default:
+        alert("Operador inválido, tente novamente!");
+        location.reload();
+        break;
+
+}
