08716a11db5d357bd3129bec90c974c23e5ee159
RhuanCheroso <rhuanbezerra12@gmail.com>
2023-12-05 16:56:42 -0300

    .

--- aula10 - Jquery Animation/materialDeAula/js/script.js
+++ aula10 - Jquery Animation/materialDeAula/js/script.js
@@ -8,6 +8,33 @@ $("#animacao2").on('click' , function(){
     animate({marginleft : "300px", rotate : "360deg"})
 })
 $("#animacao3").on('click' , function(){
-    caixa.animate({margin : "0px", rotate : "-360deg" })
-
+    caixa.animate({margin : "0px", rotate : "-360deg"},{duration : 3000})
 })
+
+$("#animacao4").on('click' , function(){
+    caixa.animate({margin : "100px", width : "600px", height : "600px"},{complete : () => {alert("Me contrata")} })
+})
+
+$("#animacao5").on('click' , function(){
+   //caixa.hide(500)
+   //caixa.slideUp(500)
+   caixa.fadeOut(500)
+})
+
+$("#animacao6").on('click' , function(){
+    //caixa.show(500)
+    //caixa.slideDown(500)
+    caixa.fadeIn(500)
+ })
+ $("#animacao7").on('click' , function(){
+    //caixa.fadeToggle(500)
+    caixa.slideToggle(500)
+ })
+
+ $("#animacao8").on('click' , function(){
+    caixa.css("transition" , "1s")
+    caixa.css("backgroundColor", "red")
+    $("img").attr("src", "https://static.significados.com.br/foto/cruz-patea.jpg")
+    $("img").attr("width", "300px")
+    $("#teste").attr("placeholder" , "Mensagem legal")
+ })
